91546aaa1c57aba1aa30471028c6c42877a4e932
Obaid Siddiqui <osiddiqui@vetstreet.com>
2013-06-17 18:38:47 -0400

    first commit

---
+++ src/main/java/com/cxf/demo/Customer.java
@@ -0,0 +1,25 @@
+package com.cxf.demo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Customer")
+public class Customer {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
